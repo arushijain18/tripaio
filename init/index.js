be3af70d08f179f317a9e24a4a001d575b33e2b2
arushijain18 <your-email@example.com>
2026-05-17 06:39:19 +0530

    Fix dotenv path in seed file

--- init/index.js
+++ init/index.js
@@ -7,7 +7,9 @@ const axios = require("axios");
 
 // THIS IS THE BASIC SETUP FOR MONGODB
 // const MONGO_URL = "mongodb://127.0.0.1:27017/tripaio";
-require("dotenv").config({ path: "../.env" });
+// require("dotenv").config({ path: "../.env" });
+require("dotenv").config({ path: require("path").resolve(__dirname, "../.env") });
+
 const MONGO_URL = process.env.ATLASDB_URL;
 
 main().then(() =>{
